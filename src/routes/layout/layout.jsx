bf6cf04e42e7da866ec5b3141113518ada100da5
Agung Ramadhan Suyono <agungramadhans.work@gmail.com>
2024-05-12 19:19:31 +0700

    11. React.js Protected Routes with React Router Dom

--- src/routes/layout/layout.jsx
+++ src/routes/layout/layout.jsx
@@ -1,18 +1,41 @@
 import "./layout.scss";
-import Navbar from "../../components/navbar/Navbar"
-import { Outlet } from "react-router-dom";
+import Navbar from "../../components/navbar/Navbar";
+import { Navigate, Outlet } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 function Layout() {
-  return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <Outlet/>
-      </div>
-    </div>
-  );
+	return (
+		<div className="layout">
+			<div className="navbar">
+				<Navbar />
+			</div>
+			<div className="content">
+				<Outlet />
+			</div>
+		</div>
+	);
 }
 
-export default Layout;
+function AuthLayout() {
+	const {currentUser} = useContext(AuthContext);
+	// const navigate = useNavigate();
+
+	if(!currentUser) {
+		// navigate('/login');
+		return <Navigate to="/login" />
+	}
+
+	return (
+		<div className="layout">
+			<div className="navbar">
+				<Navbar />
+			</div>
+			<div className="content">
+				<Outlet />
+			</div>
+		</div>
+	);
+}
+
+export {Layout, AuthLayout};
